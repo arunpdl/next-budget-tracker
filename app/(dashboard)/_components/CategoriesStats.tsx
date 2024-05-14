2618b9be6c91b593cba1f5faa21d5c61a12c525a
Arun Poudel <arunpdl013@gmail.com>
2024-05-14 22:54:16 +0545

    Add stats to dashboard

---
+++ app/(dashboard)/_components/CategoriesStats.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import { GetCategoriesStatsResponseType } from "@/app/api/stats/categories/route";
+import SkeletonWrapper from "@/components/SkeletonWrapper";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { DateToUTCDate, GetFormatterFromCurrency } from "@/lib/helpers";
+import { TransactionType } from "@/lib/types";
+import { UserSettings } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+
+interface Props {
+  userSettings: UserSettings;
+  from: Date;
+  to: Date;
+}
+
+function CategoriesStats({ userSettings, from, to }: Props) {
+  const statsQuery = useQuery<GetCategoriesStatsResponseType>({
+    queryKey: ["overview", "stats", "categories", from, to],
+    queryFn: () =>
+      fetch(
+        `/api/stats/categories?from=${DateToUTCDate(from)}&to=${DateToUTCDate(
+          to
+        )}`
+      ).then((res) => res.json()),
+  });
+
+  const formatter = useMemo(() => {
+    return GetFormatterFromCurrency(userSettings.currency);
+  }, [userSettings.currency]);
+
+  return (
+    <div className="relative flex flex-wrap gap-2 md:flex-nowrap">
+      <SkeletonWrapper isLoading={statsQuery.isFetching}>
+        <CategoriesCard
+          formatter={formatter}
+          stats={statsQuery.data || []}
+          type="income"
+        />
+      </SkeletonWrapper>
+      <SkeletonWrapper isLoading={statsQuery.isFetching}>
+        <CategoriesCard
+          formatter={formatter}
+          stats={statsQuery.data || []}
+          type="expense"
+        />
+      </SkeletonWrapper>
+    </div>
+  );
+}
+
+export default CategoriesStats;
+
+function CategoriesCard({
+  formatter,
+  stats,
+  type,
+}: {
+  formatter: Intl.NumberFormat;
+  stats: GetCategoriesStatsResponseType;
+  type: TransactionType;
+}) {
+  const filteredData = stats.filter((stat) => stat.type === type);
+  const total = filteredData.reduce(
+    (acc, el) => acc + (el._sum?.amount || 0),
+    0
+  );
+
+  return (
+    <Card className="h-80 w-full">
+      <CardHeader>
+        <CardTitle className="grid grid-flow-row justify-between gap-2 text-muted-foreground md:grid-flow-col">
+          {type === "income" ? "Incomes" : "Expenses"} by category
+        </CardTitle>
+      </CardHeader>
+      <div className="flex items-center justify-between gap-2">
+        {filteredData.length === 0 && (
+          <div className="flex h-60 w-full flex-col items-center justify-center">
+            No data for the selected period
+            <p className="text-sm text-muted-foreground">
+              Try selecting a different period or try adding new{" "}
+              {type === "income" ? "incomes" : "expenses"}
+            </p>
+          </div>
+        )}
+        {filteredData?.length > 0 && (
+          <ScrollArea className="h-60 w-full px-4">
+            <div className="flex flex-col gap-4 p-4">
+              {filteredData.map((item) => {
+                const amount = item._sum?.amount || 0;
+                const percentage = (amount * 100) / (total || amount);
+                return (
+                  <div key={item.category} className="flex flex-col gap-2">
+                    <div className="flex items-center justify-between">
+                      <span className="flex items-center text-gray-400">
+                        {item.categoryIcon} {item.category}
+                        <span className="ml-2 text-xs text-muted-foreground">
+                          ({percentage.toFixed(0)}%)
+                        </span>
+                      </span>
+                      <span className="text-sm text-gray-400">
+                        {formatter.format(amount)}
+                      </span>
+                    </div>
+                    <Progress
+                      value={percentage}
+                      indicator={
+                        type === "income" ? "bg-emerald-500" : "bg-rose-500"
+                      }
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </ScrollArea>
+        )}
+      </div>
+    </Card>
+  );
+}
---
+++ app/(dashboard)/_components/CreateCategoryDialog.tsx
@@ -0,0 +1,216 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { TransactionType } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import {
+  CreateCategorySchema,
+  CreateCategorySchemaType,
+} from "@/schema/categories";
+import EmojiData from "@emoji-mart/data";
+import EmojiPicker from "@emoji-mart/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Category } from "@prisma/client";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { CircleOff, Loader2, PlusSquare } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useCallback, useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { CreateCategory } from "../_actions/categories";
+
+interface Props {
+  type: TransactionType;
+  onSuccessCallback: (category: Category) => void;
+}
+
+function CreateCategoryDialog({ type, onSuccessCallback }: Props) {
+  const [open, setOpen] = useState(false);
+  const form = useForm<CreateCategorySchemaType>({
+    resolver: zodResolver(CreateCategorySchema),
+    defaultValues: {
+      type,
+    },
+  });
+
+  const queryClient = useQueryClient();
+  const theme = useTheme();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: CreateCategory,
+    onSuccess: async (data: Category) => {
+      form.reset({
+        name: "",
+        icon: "",
+        type,
+      });
+      toast.success(`Category ${data.name} created successfully 🎉`, {
+        id: "create-category",
+      });
+      await queryClient.invalidateQueries({
+        queryKey: ["categories"],
+      });
+      onSuccessCallback(data);
+      setOpen((prev) => !prev);
+    },
+    onError: () => {
+      toast.error("Something went wrong, please try again later", {
+        id: "create-category",
+      });
+    },
+  });
+
+  const onSubmit = useCallback(
+    (values: CreateCategorySchemaType) => {
+      toast.loading("Creating category...", {
+        id: "create-category",
+      });
+      mutate(values);
+    },
+    [mutate]
+  );
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant={"ghost"}
+          className="flex border-separate items-center justify-start rounded-none border-b px-3 py-3 text-muted-foreground"
+        >
+          <PlusSquare className="mr-2 h-4 w-4" />
+          Create new
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            Create{" "}
+            <span
+              className={cn(
+                "m-1",
+                type === "income" ? "text-emerald-500" : "text-rose-500"
+              )}
+            >
+              {type}
+            </span>{" "}
+            category
+          </DialogTitle>
+          <DialogDescription>
+            Categories are used to group your transactions.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Category"
+                      defaultValue={""}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    This is how your category will appear in the app
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="icon"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Icon</FormLabel>
+                  <FormControl>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button variant="outline" className="h-[100px] w-full">
+                          {form.watch("icon") ? (
+                            <div className="flex flex-col items-center gap-2">
+                              <span className="text-5xl" role="img">
+                                {field.value}
+                              </span>
+                              <p className="text-xs text-muted-foreground">
+                                Click to change
+                              </p>
+                            </div>
+                          ) : (
+                            <div className="flex flex-col items-center gap-2">
+                              <CircleOff className="h-[48px] w-[48px]" />
+                              <p className="text-xs text-muted-foreground">
+                                Click to select
+                              </p>
+                            </div>
+                          )}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-full">
+                        <EmojiPicker
+                          data={EmojiData}
+                          onEmojiSelect={(emoji: { native: string }) => {
+                            field.onChange(emoji.native);
+                          }}
+                          theme={theme.resolvedTheme}
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </FormControl>
+                  <FormDescription>
+                    This is how your category will appear in the app
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button
+              type="button"
+              variant={"secondary"}
+              onClick={() => {
+                form.reset();
+              }}
+            >
+              Cancel
+            </Button>
+          </DialogClose>
+          <Button onClick={form.handleSubmit(onSubmit)} disabled={isPending}>
+            {!isPending ? "Create" : <Loader2 className="animate-spin" />}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default CreateCategoryDialog;
